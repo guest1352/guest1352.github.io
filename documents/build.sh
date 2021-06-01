ecf9260c34a0da7d50f95acc98822690a7d677fa
guest1352 <publicshitpost@gmail.com>
2021-06-01 18:15:52 +0200

    torrenting guide started cuz lol

--- documents/build.sh
+++ documents/build.sh
@@ -3,15 +3,23 @@ main() {
 	opt=$( tr '[:upper:]' '[:lower:]' <<<"$1" )
 	case "$opt" in
 		"htmld")
-			echo "Compiling HTMLD"; htmldCompile;;
+			htmldCompile;;
+		"torrent")
+			torrentCompile;;
 		*)
-			echo "Compiling all"; htmldCompile;;
+			echo "Compiling all"; htmldCompile; torrentCompile;;
 	esac
 }
 htmldCompile() {
+	echo "Compiling HTMLD"
 	for i in 1 2; do ( cd src && hevea HTMLD.tex -o ../HTMLD/index.html ); done
 	printf \n\n | imagen HTMLD/index
 	cp src/HelloLinuxWorld.png HTMLD/index001.png
 	rm -f HTMLD/*.haux HTMLD/*.htoc HTMLD/*.image*
+}
+torrentCompile() {
+	echo "Compiling TORRENT"
+	for i in 1 2; do ( cd src && hevea TORRENT.tex -o ../TORRENT/index.html ); done
+	rm -f TORRENT/*.haux TORRENT/*.htoc TORRENT/*.image*
 }
 main "$@"; exit
