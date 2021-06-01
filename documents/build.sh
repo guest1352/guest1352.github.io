b12cb1c50024d501ceb1cc0a58b4b0c407b17802
guest1352 <publicshitpost@gmail.com>
2021-06-01 16:37:49 +0200

    Bash

--- documents/build.sh
+++ documents/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-for i in 1 2; do hevea src/HTMLD.tex -o HTMLD/index.html; done
-printf \n\n | imagen HTMLD/index
-cp src/HelloLinuxWorld.png HTMLD/index001.png
-rm -f HTMLD/*.haux HTMLD/*.htoc HTMLD/*.image*
+main() {
+	opt=$( tr '[:upper:]' '[:lower:]' <<<"$1" )
+	case "$opt" in
+		"htmld")
+			echo "Compiling HTMLD"; htmldCompile;;
+		*)
+			echo "Compiling all"; htmldCompile;;
+	esac
+}
+htmldCompile() {
+	for i in 1 2; do ( cd src && hevea HTMLD.tex -o ../HTMLD/index.html ); done
+	printf \n\n | imagen HTMLD/index
+	cp src/HelloLinuxWorld.png HTMLD/index001.png
+	rm -f HTMLD/*.haux HTMLD/*.htoc HTMLD/*.image*
+}
+main "$@"; exit
