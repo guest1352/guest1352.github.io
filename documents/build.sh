91bb2d6919ffaeb1365a99f845aec8f8b95ebbb4
guest1352 <publicshitpost@gmail.com>
2021-06-05 01:33:37 +0200

    changed Other Documentation to Sources in TORRENT doc

--- documents/build.sh
+++ documents/build.sh
@@ -12,14 +12,14 @@ main() {
 }
 htmldCompile() {
 	echo "Compiling HTMLD"
-	for i in 1 2; do ( cd src && hevea HTMLD.tex -o ../HTMLD/index.html ); done
+	for i in 1 2; do ( cd src && hevea HTMLD.tex -O -o ../HTMLD/index.html ); done
 	printf \n\n | imagen HTMLD/index
 	cp src/HelloLinuxWorld.png HTMLD/index001.png
 	rm -f HTMLD/*.haux HTMLD/*.htoc HTMLD/*.image*
 }
 torrentCompile() {
 	echo "Compiling TORRENT"
-	for i in 1 2; do ( cd src && hevea TORRENT.tex -o ../TORRENT/index.html ); done
+	for i in 1 2; do ( cd src && hevea TORRENT.tex -O -o ../TORRENT/index.html ); done
 	rm -f TORRENT/*.haux TORRENT/*.htoc TORRENT/*.image*
 }
 main "$@"; exit
