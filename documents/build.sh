051e99003dba6608d830bfa28767fe3031855f1e
guest1352 <publicshitpost@gmail.com>
2021-06-06 01:51:21 +0200

    pog pog?!?! auto dates?!?!

--- documents/build.sh
+++ documents/build.sh
@@ -6,20 +6,33 @@ main() {
 			htmldCompile;;
 		"torrent")
 			torrentCompile;;
+		"qemu")
+			qemuCompile;;
 		*)
-			echo "Compiling all"; htmldCompile; torrentCompile;;
+			echo "Compiling all"; htmldCompile; torrentCompile; qemuCompile;;
 	esac
 }
 htmldCompile() {
 	echo "Compiling HTMLD"
+	date="$(date -I -r src/HTMLD.tex | sed 's/-/ /g')"
 	for i in 1 2; do ( cd src && hevea HTMLD.tex -O -o ../HTMLD/index.html ); done
 	printf \n\n | imagen HTMLD/index
 	cp src/HelloLinuxWorld.png HTMLD/index001.png
 	rm -f HTMLD/*.haux HTMLD/*.htoc HTMLD/*.image*
+	sed -i "s/DATE/$date/g" HTMLD/index.html
 }
 torrentCompile() {
 	echo "Compiling TORRENT"
+	date="$(date -I -r src/TORRENT.tex | sed 's/-/ /g')"
 	for i in 1 2; do ( cd src && hevea TORRENT.tex -O -o ../TORRENT/index.html ); done
 	rm -f TORRENT/*.haux TORRENT/*.htoc TORRENT/*.image*
+	sed -i "s/DATE/$date/g" TORRENT/index.html
+}
+qemuCompile() {
+	echo "Compiling QEMU"
+	date="$(date -I -r src/QEMU.tex | sed 's/-/ /g')"
+	for i in 1 2; do ( cd src && hevea QEMU.tex -O -o ../QEMU/index.html ); done
+	rm -f QEMU/*.haux QEMU/*.htoc QEMU/*.image*
+	sed -i "s/DATE/$date/g" QEMU/index.html
 }
 main "$@"; exit
